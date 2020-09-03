85e32fd0fee81facfb7a9c95cfac7284921c6a89
ksyno <ksyno@KYNO>
2020-09-03 10:28:56 +0200

    Ready for tests

--- src/main/java/com/ksynowiec/forum/dao/ForumDaoImp.java
+++ src/main/java/com/ksynowiec/forum/dao/ForumDaoImp.java
@@ -5,12 +5,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ksynowiec.forum.dto.AnswerDTO;
@@ -21,10 +19,7 @@ import com.ksynowiec.forum.model.Post;
 
 @Repository
 public class ForumDaoImp implements ForumDao {
-    
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-    
+
     @PersistenceContext
     EntityManager entityManager;
     
